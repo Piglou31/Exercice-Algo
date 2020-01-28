27844510916f77b1b5d237b6f94b1d26dd737afb
Paletou <tom.paletou@gmail.com>
2020-01-28 18:42:54 +0100

    Healer

--- Exercice.c
+++ Exercice.c
@@ -32,6 +32,10 @@ int main (){
 		int provoc = 0;
 		int decisionT = 0;
 		
+	//Stats Healer
+		int pvHealer = 100;
+		int decisionH = 0;
+		
 		
 	srand(time(NULL));
 
@@ -39,7 +43,7 @@ int main (){
 		
 		
 		//Tour du Tank
-		if (pvTank>0)
+		if (pvTank>0){
 			
 		printf ("Tour du Tank :Voulez vous attaquer (1) vous defendre (2) ou provoquer les monstres (3) ?\n");
 		scanf("%d",&decisionT);
@@ -106,7 +110,7 @@ int main (){
 			attaquemonstre = 10 ;
 			attaquejoueur1 = 10;
 			attaquejoueur2 = 10;
-			
+		}
 			
 			
 			
@@ -135,9 +139,6 @@ int main (){
 			poisonA ++;
 			PMAlch = PMAlch -3;
 				}
-		
-				
-			
 				
 			if (decisionA == 2){
 			printf("Vous decidez de vous defendre.\n");
@@ -169,22 +170,22 @@ int main (){
 			}
 			
 			if(decisionM1 == 1 && provoc == 1){
-			printf("Le monstre attaque le Tank.\n");
+			printf("Le monstre 1 attaque le Tank.\n");
 			pvTank = pvTank - attaquemonstre;
 				}
 				
 			if(decisionM1 == 1 && provoc == 0){
-			printf("Le monstre vous attaque.\n");
+			printf("Le monstre 1 vous attaque.\n");
 			pvAlch = pvAlch - attaquemonstre;
 				}
 				
 			if(decisionM2 == 1 && provoc == 1){
-			printf("Le monstre attaque le Tank.\n");
+			printf("Le monstre 2 attaque le Tank.\n");
 			pvTank = pvTank - attaquemonstre;
 				}
 				
 			if(decisionM2 == 1 && provoc == 0){
-			printf("Le monstre vous attaque.\n");
+			printf("Le monstre 2 vous attaque.\n");
 			pvAlch = pvAlch - attaquemonstre;
 				}
 		
@@ -216,6 +217,112 @@ int main (){
 			attaquejoueur2 = 10;
 		}
 		
+		
+		//Tour du Healer
+		if (pvTank>0){
+			
+		printf ("Tour du Healer :Voulez vous attaquer (1) vous defendre (2) ou soigner un allié (3) ?\n");
+		scanf("%d",&decisionH);
+		decisionM1 =((rand()%3)+1);
+		decisionM2 =((rand()%3)+1);
+
+			
+			if (decisionH == 2){
+			printf("Vous decidez de vous defendre.\n");
+			attaquemonstre = attaquemonstre /4;
+				}
+				
+				
+			if(decisionH == 1){
+			printf("Vous decidez d'attaquer.\n");
+			printf("Quelmonstre voulez-vous attaquer ? (1) ou (2)\n");
+			scanf("%d",&Monstre);
+				if (decisionM2 == 2){
+					printf("Le monstre 2 se defend.\n");
+					attaquejoueur2 = attaquejoueur2 /4;		
+				}
+				if (decisionM1 == 2){
+					printf("Le monstre 1 se defend.\n");
+					attaquejoueur1 = attaquejoueur1 /4;		
+				}
+				
+				if(Monstre == 1){
+					printf("Vous decidez d'attaquer le monstre 1.\n");
+					pvmonstre1 = pvmonstre1 - attaquejoueur1;
+				}	
+				if (Monstre == 2 ){
+					printf("Vous decidez d'attaquer le monstre 2.\n");
+					pvmonstre2 = pvmonstre2 - attaquejoueur2;
+				}
+			}
+			if(decisionM1 == 1 && provoc == 0){
+			printf("Le monstre 1 vous attaque.\n");
+			pvHealer = pvHealer - attaquemonstre;
+				}
+			
+			if(decisionM1 == 1 && provoc == 1){
+			printf("Le monstre attaque le Tank.\n");
+			pvTank = pvTank - attaquemonstre;
+				}
+				
+			if(decisionM2 == 1){
+			printf("Le monstre 2 vous attaque.\n");
+			pvHealer = pvHealer - attaquemonstre;
+				}
+				
+			if(decisionM2 == 1 && provoc == 1){
+			printf("Le monstre 2 attaque le Tank.\n");
+			pvTank = pvTank - attaquemonstre;
+				}
+				
+			if(decisionH == 3 ){
+			printf ("Qui voulez vous soigner? Vous (1), Le Tank (2), L'alchimiste (3), ou le mage (4)\n");
+			scanf ("%d",&Soin);
+			
+				if (soin == 1){
+					pvHealer = pvHealer + 15 ;
+					if ( pvHealer >100)
+						pvHealer = 100;
+				}
+				if (soin == 2){
+					pvTank = pvTank + 15 ;
+					if ( pvTank >100)
+						pvTank = 100;
+				}
+				if (soin == 3){
+					pvAlch = pvAlch + 15 ;
+					if ( pvAlch >100)
+						pvAlch = 100;
+				}
+				if (soin == 4){
+					pvMage = pvMage + 15 ;
+					if ( pvMage >100)
+						pvMage = 100;
+				}
+				else {
+					printf ("Qui voulez vous soigner? Vous (1), Le Tank (2), L'alchimiste (3), ou le mage (4)\n");
+			scanf ("%d",&Soin); 
+				}
+			
+			}	
+				
+				if (decisionM1 == 3){
+			printf("Le monstre 1 vous empoisonne.\n");
+			poisonM ++;
+				}
+				
+			if (decisionM2 == 3){
+			printf("Le monstre 2 vous empoisonne.\n");
+			poisonM ++;
+				}
+			
+			printf ("Le monstre 1 a %d points de vie.\n", pvmonstre1);
+			printf ("Le monstre 2 a %d points de vie.\n", pvmonstre2);
+			printf ("Il vous reste %d points de vie.\n", pvHealer);
+			attaquemonstre = 10 ;
+			attaquejoueur1 = 10;
+			attaquejoueur2 = 10;
+		}
 			
 			
 	
